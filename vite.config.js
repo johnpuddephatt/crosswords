ea4b2455cbb4ea152890d9cbb0f9ccac53af6f9a
John Puddephatt <john@jdp.org.uk>
2023-09-17 11:32:46 +0100

    autoprefixer for android 4

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,29 @@
 import legacy from '@vitejs/plugin-legacy'
+import autoprefixer from 'autoprefixer'
 
 // vite.config.js
 export default {
-     plugins: [
-    legacy({
-      targets: ['Android 4'],
-    }),
-  ],
- 
+    plugins: [
+        
+        // autoprefixer({
+        //     overrideBrowserslist: [
+        //         'Android 4',
+        //     ]
+        // }),
+        legacy({
+            targets: ['Android 4'],
+        }),
+    ],
+    css: {
+    
+    postcss: {
+      plugins: [
+          autoprefixer({
+            overrideBrowserslist: [
+                'Android 4',
+            ]
+        }),
+      ],
+    }
+  },
 }
